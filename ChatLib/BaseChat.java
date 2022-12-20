684a770a9e512fc99cc6eea2cc7b21e683b54711
Anatoly <107132732+Seven7897@users.noreply.github.com>
2022-12-20 19:43:26 +0300

    Update BaseChat.java

--- ChatLib/BaseChat.java
+++ ChatLib/BaseChat.java
@@ -1,3 +1,15 @@
-public class BaseChat {
-    
+package MessengerApp.ChatLib;
+
+import MessengerApp.User;
+
+public interface BaseChat {
+    void addUser(User user);
+
+    void removeUser(User user);
+
+    void printMessage(User user, String msg);
+
+    void printUsers();
+
+    void getHistory();
 }
